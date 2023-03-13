ec1937c30725f7dcea4ff7f54f93f447e0a93fcc
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-03-13 14:27:50 +0300

    Sketch of Node

--- src/main/java/lab1/stack/node/Node.java
+++ src/main/java/lab1/stack/node/Node.java
@@ -1,4 +1,16 @@
 package lab1.stack.node;
 
-public class Node {
+public class Node<TInfo> {
+    TInfo info;
+    Node<TInfo> next;
+    Node(TInfo elem, Node<TInfo> next)
+    {
+        info = elem;
+        this.next = next;
+    }
+    Node(TInfo elem)
+    {
+        //this.next = null;
+        info = elem;
+    }
 }
